f262168a7904f60f5d280a0679b0d3a899488915
efosdan <efosdan@yahoo.com>
2022-09-14 22:58:39 +0200

    added route to app js

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ app.locals.appTitle = `${capitalized(projectName)} created with IronLauncher`;
 const index = require("./routes/index.routes");
 app.use("/", index);
 
+const authRouter = require("./routes/auth.routes"); // <== has to be added
+app.use("/", authRouter);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
